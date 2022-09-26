d41fbaac5c5d15aefd711395635a8ddf66624d4b
aboueloa <ayman.aboueloula@grenoble-inp.org>
2022-09-26 21:59:43 +0200

    Implement withdrawal service

--- src/main/java/com/codebusters/bankaccountkata/domain/service/BankAccountService.java
+++ src/main/java/com/codebusters/bankaccountkata/domain/service/BankAccountService.java
@@ -14,6 +14,6 @@ public class BankAccountService {
     }
 
     public Operation withdrawal(Transaction transaction) {
-        return null;
+        return bankAccountPort.withdrawMoney(transaction);
     }
 }
